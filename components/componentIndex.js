f595e18945c533625f069065f6e630c9845e4526
Ankan-07-Dutta <bhaskardutta335@gmail.com>
2025-08-21 12:25:53 +0530

    updated subscribe module

--- components/componentIndex.js
+++ components/componentIndex.js
@@ -4,6 +4,7 @@ import Footer from "./Footer/Footer";
 import HeroSection from "./HeroSection/HeroSection";
 import Service from "./Service/Service";
 import BigNFTSlider from "./BigNFTSlider/BigNFTSlider";
+import Subscribe from "./Subscribe/Subscribe";
 
 
-export {Button, Footer , Navbar, HeroSection , Service, BigNFTSlider };
+export { Button, Footer, Navbar, HeroSection, Service, BigNFTSlider, Subscribe };
